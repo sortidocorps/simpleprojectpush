fe9d7ff5442efb731a143fc515ddd149fc5e8b70
negima <sortidocorp@gmail.com>
2023-08-01 23:01:59 +0200

    Message du commit quotidien

--- append.md
+++ append.md
@@ -1,2 +1,3 @@
 
 Modification quotidienne.
+Modification quotidienne du 2023-08-01 à 23:01:59.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
+const moment = require("moment");
 
 const repoPath = "/"; // Remplacez par le chemin absolu de votre répertoire Git local
 const commitMessage = "Message du commit quotidien";
@@ -8,20 +9,27 @@ const filePath = "append.md"; // Chemin relatif vers le fichier que vous souhait
 
 // Fonction pour effectuer le commit et le push
 function commitAndPush() {
-  try {
-    // Modifier le fichier
-    fs.appendFileSync(filePath, "\nModification quotidienne.");
+    try {
+      // Obtenir la date et l'heure actuelles
+      const currentDate = moment().format("YYYY-MM-DD");
+      const currentTime = moment().format("HH:mm:ss");
+        
+      // Modifier le fichier
+      fs.appendFileSync(
+        filePath,
+        `\nModification quotidienne du ${currentDate} à ${currentTime}.`
+      );
 
-    // Ajouter les modifications et faire un commit
-    //execSync(`cd ${repoPath} && git add . && git commit -m "${commitMessage}"`);
-    execSync(`git add . && git commit -m "${commitMessage}"`);
+      // Ajouter les modifications et faire un commit
+      //execSync(`cd ${repoPath} && git add . && git commit -m "${commitMessage}"`);
+      execSync(`git add . && git commit -m "${commitMessage}"`);
 
-    // Push vers la branche spécifiée
-    //execSync(`cd ${repoPath} && git push origin ${branchName}`);
-    execSync(`git push`);
+      // Push vers la branche spécifiée
+      //execSync(`cd ${repoPath} && git push origin ${branchName}`);
+      execSync(`git push`);
 
-    console.log("Commit and push successful.");
-  } catch (error) {
+      console.log("Commit and push successful.");
+    } catch (error) {
     console.error("Error occurred:", error);
   }
 }
